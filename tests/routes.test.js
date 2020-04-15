628ba282aa10928736f3db07e6829c40b63de49e
Roger Heathcote <git@technicalbloke.com>
2020-04-15 16:01:31 +0100

    Over to Kat!

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,8 +1,19 @@
+const supertest = require("supertest");
 const test = require("tape");
+const server = require("../server");
 
 test("Route tests are running!", (t) => {
     const x = 5;
     t.equal(x, 5, "this is working");
     t.end();
-  });
-  
+});
+
+test("Test main route returns 200", t => {
+    supertest(server)
+        .get("/")
+        .expect(200)
+        .end( (err,res) => {
+            t.error(err);
+            t.end();
+        })
+})
